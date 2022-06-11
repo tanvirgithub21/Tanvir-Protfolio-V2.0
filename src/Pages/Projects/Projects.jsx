906b86fd59559a90fd46c001dd586daa79554cda
Tanvir Ahmed <tanvir.be.global@gmail.com>
2022-06-12 02:23:31 +0600

    projects daynamic

--- src/Pages/Projects/Projects.jsx
+++ src/Pages/Projects/Projects.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SingleProject from "./SingleProject";
+import { toast } from "react-toastify";
 
 const Projects = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      "https://raw.githubusercontent.com/tanvirgithub21/assainment-11-data/main/portfolio.json"
+    )
+      .then((res) => res.json())
+      .then((data) => setData(data))
+      .catch((err) => {
+        err && toast.error("Data not found");
+      });
+  }, []);
+
   return (
     <div>
       <div className="container">
@@ -11,9 +25,9 @@ const Projects = () => {
         </h2>
 
         <div className="projectsBox my-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 sm:gap-6 md:gap-10">
-          <SingleProject />
-          <SingleProject />
-          <SingleProject />
+          {data.map((singleData) => (
+            <SingleProject key={singleData._id} singleData={singleData} />
+          ))}
         </div>
       </div>
     </div>
--- src/Pages/Projects/SingleProject.jsx
+++ src/Pages/Projects/SingleProject.jsx
@@ -2,58 +2,60 @@ import React from "react";
 import { Link } from "react-router-dom";
 import project1 from "../../Images/project-1.png";
 
-const SingleProject = () => {
+const SingleProject = ({ singleData }) => {
+  const { fractures } = singleData;
+  console.log(singleData?.projLinks);
+
   return (
     <div class=" w-full bg-white rounded-lg border border-gray-200 shadow-md mb-8 md:mb-0">
       <img class="rounded-t-lg" src={project1} alt="img" />
       <div class="p-5">
-        <h2 className="text-2xl font-[600]">3B Manufacturer</h2>
-        <h4 className="text-sm text-gray-500">tools manufacturer warehouse</h4>
+        <h2 className="text-2xl font-[600]">{singleData?.projName}</h2>
+        <h4 className="text-sm text-gray-500">{singleData?.projShortDric}</h4>
         <p className="mt-2 text-md text-gray-600">Features</p>
         <ul className="list-disc ml-[1.2rem] text-sm mt-1 text-gray-600">
-          <li>Login Email and password & Google</li>
-          <li>Full Responsive website mobile tablet desktop</li>
-          <li>Use JWT verify user</li>
+          {fractures?.map((singleFractures) => (
+            <li>{singleFractures}</li>
+          ))}
         </ul>
         <div className="language mt-4 flex flex-wrap">
-          <span className="px-2 py-1 mt-2 bg-slate-600 text-white text-[500] text-sm mr-2 rounded-md">
-            HTML
-          </span>
-          <span className="px-2 py-1 mt-2 bg-slate-600 text-white text-[500] text-sm mr-2 rounded-md">
-            CSS
-          </span>
-          <span className="px-2 py-1 mt-2 bg-slate-600 text-white text-[500] text-sm mr-2 rounded-md">
-            Javascript
-          </span>
-          <span className="px-2 py-1 mt-2 bg-slate-600 text-white text-[500] text-sm mr-2 rounded-md">
-            React
-          </span>
-          <span className="px-2 py-1 mt-2 bg-slate-600 text-white text-[500] text-sm mr-2 rounded-md">
-            MERN
-          </span>
+          {singleData?.projUsedLanguage?.map((language) => (
+            <span className="px-2 py-1 mt-2 bg-slate-600 text-white text-[500] text-sm mr-2 rounded-md">
+              {language}
+            </span>
+          ))}
         </div>
 
         <div className="links mt-6 flex flex-wrap">
-          <a
-            href=""
-            className="px-2 py-1 mt-2 bg-amber-500 text-gray-100 text-[500] text-sm mr-2 rounded-md"
-          >
-            Preview
-          </a>
-          <a
-            href=""
-            className="px-2 py-1 mt-2 bg-amber-500 text-gray-100 text-[500] text-sm mr-2 rounded-md"
-          >
-            Git Client
-          </a>
-          <a
-            href=""
-            className="px-2 py-1 mt-2 bg-amber-500 text-gray-100 text-[500] text-sm mr-2 rounded-md"
-          >
-            Git Server
-          </a>
+          {singleData?.projLinks?.liveSite && (
+            <a
+              href={singleData?.projLinks?.liveSite}
+              className="px-2 py-1 mt-2 bg-amber-500 text-gray-100 text-[500] text-sm mr-2 rounded-md"
+            >
+              Preview
+            </a>
+          )}
+
+          {singleData?.projLinks?.clientSideLink && (
+            <a
+              href={singleData?.projLinks?.clientSideLink}
+              className="px-2 py-1 mt-2 bg-amber-500 text-gray-100 text-[500] text-sm mr-2 rounded-md"
+            >
+              Git Client
+            </a>
+          )}
+
+          {singleData?.projLinks?.serverSiteLink && (
+            <a
+              href={singleData?.projLinks?.serverSiteLink}
+              className="px-2 py-1 mt-2 bg-amber-500 text-gray-100 text-[500] text-sm mr-2 rounded-md"
+            >
+              Git Server
+            </a>
+          )}
+
           <Link
-            to=""
+            to={`/projects/:${singleData?._id}`}
             className="px-2 py-1 mt-2 bg-amber-500 text-gray-100 text-[500] text-sm mr-2 rounded-md"
           >
             Details
