adc9dc7ca65114fb0d8641613888237c70a6ed40
Tanvir Ahmed <tanvir.be.global@gmail.com>
2022-06-11 19:59:26 +0600

    added responsive contact us

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -39,12 +39,12 @@ const Contact = () => {
   return (
     <div className="container">
       {/* section header about me  */}
-      <h2 className="text-2xl text-center font-semibold mt-5">
+      <h2 className="text-2xl text-center font-semibold mt-5  mb-7 md:mb-0">
         <span className="text-emerald-400">Contact</span> US
       </h2>
-      <div className="w-full h-[calc(100vh-55px)] flex justify-center items-center">
-        <div className="w-[60rem] h-[28rem] flex shadow-b rounded-2xl overflow-hidden">
-          <div className="w-[65%] contactUs  bg-[#FFFF] p-6">
+      <div className="w-full h-[calc(100vh-55px)] md:flex justify-center items-center">
+        <div className="w-full md:w-[60rem] md:flex shadow-b rounded-2xl overflow-hidden">
+          <div className="w-full md:w-[65%] h-[28rem] contactUs  bg-[#FFFF] p-6">
             <h2 className="text-xl font-[500] mb-5">Send us a Massages</h2>
 
             <form onSubmit={handleSubmit(onSubmit)} className="">
@@ -90,55 +90,59 @@ const Contact = () => {
               </div>
             </form>
           </div>
-          <div className="w-[35%] contactInfo bg-[#143d6c] p-4">
-            <h4 className=" text-lg font-[500] text-slate-100 mb-5">
-              Contact Information
-            </h4>
+          <div className="min-w-full md:w-[35%] md:min-w-[18rem] h-[28rem] contactInfo bg-[#143d6c] p-4 text-center md:text-left">
+            <div className="">
+              <h4 className=" text-lg font-[500] text-slate-100 mb-5">
+                Contact Information
+              </h4>
 
-            <ul className="text-gray-100">
-              <li className="mb-5 flex items-center">
-                <FaUserCircle className="mr-2" /> Md Tanvir Ahmed
-              </li>
-              <li className="mb-5 flex items-center">
-                <FaLaptopCode className="mr-2" />
-                Junior Web Devloper
-              </li>
-              <li className="mb-5 flex items-center">
-                <FaPhoneAlt className="mr-2" />
-                +880 1845238826
-              </li>
-              <li className="mb-5 flex items-center">
-                <FaRegEnvelope className="mr-2" />
-                tanvir.bd.global@gmail.com
-              </li>
-              <li className="mb-5 flex items-center">
-                <FaLocationArrow className="mr-2" />
-                Banugonj, Barisal, Bangladesh
-              </li>
-            </ul>
+              <div className="linkBox flex md:block justify-center items-center  ">
+                <ul className="text-gray-100">
+                  <li className="mb-5 flex items-center">
+                    <FaUserCircle className="mr-2" /> Md Tanvir Ahmed
+                  </li>
+                  <li className="mb-5 flex items-center">
+                    <FaLaptopCode className="mr-2" />
+                    Junior Web Devloper
+                  </li>
+                  <li className="mb-5 flex items-center">
+                    <FaPhoneAlt className="mr-2" />
+                    +880 1845238826
+                  </li>
+                  <li className="mb-5 flex items-center">
+                    <FaRegEnvelope className="mr-2" />
+                    tanvir.bd.global@gmail.com
+                  </li>
+                  <li className="mb-5 flex items-center">
+                    <FaLocationArrow className="mr-2" />
+                    Banugonj, Barisal, Bangladesh
+                  </li>
+                </ul>
+              </div>
 
-            <div className="socialIcon">
-              <ul className="flex justify-center sm:justify-start text-2xl text-gray-200">
-                <li className="mr-5 mt-8">
-                  <Link to="#">
-                    <FaLinkedin />
-                  </Link>
-                </li>
-                <li className="mr-5 mt-8">
-                  <Link to="#"></Link>
-                  <FaGithub />
-                </li>
-                <li className="mr-5 mt-8">
-                  <Link to="#">
-                    <FaFacebookSquare />
-                  </Link>
-                </li>
-                <li className="mr-5 mt-8">
-                  <Link to="#">
-                    <FaInstagram />
-                  </Link>
-                </li>
-              </ul>
+              <div className="socialIcon flex justify-center items-center">
+                <ul className="flex justify-center sm:justify-start text-2xl text-gray-200">
+                  <li className="mr-5 mt-8">
+                    <Link to="#">
+                      <FaLinkedin />
+                    </Link>
+                  </li>
+                  <li className="mr-5 mt-8">
+                    <Link to="#"></Link>
+                    <FaGithub />
+                  </li>
+                  <li className="mr-5 mt-8">
+                    <Link to="#">
+                      <FaFacebookSquare />
+                    </Link>
+                  </li>
+                  <li className="mr-5 mt-8">
+                    <Link to="#">
+                      <FaInstagram />
+                    </Link>
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
